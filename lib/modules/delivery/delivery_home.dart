b17d941270cb8e0ed0c9e3338c782d6fdc8d9da0
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 12:54:50 +0530

    Update delivery_home.dart

--- lib/modules/delivery/delivery_home.dart
+++ lib/modules/delivery/delivery_home.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/delivery_provider.dart';
+
+class DeliveryHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final deliveryProvider = Provider.of<DeliveryProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Delivery Dashboard')),
+      body: ListView.builder(
+        itemCount: deliveryProvider.orders.length,
+        itemBuilder: (_, index) {
+          final order = deliveryProvider.orders[index];
+          return ListTile(
+            title: Text('Order ID: ${order['id']}'),
+            subtitle: Text('Status: ${order['status']}'),
+            trailing: ElevatedButton(
+              child: Text('Mark Delivered'),
+              onPressed: () {
+                deliveryProvider.updateStatus(order['id'], 'Delivered');
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
 
