f157277e4d366128868081e273297b03decd572d
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 12:44:21 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'providers/delivery_provider.dart';
+import 'modules/delivery/delivery_home.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,6 +12,21 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  await dotenv.load(fileName: ".env");
+  runApp(DeliveryApp());
 }
 
+class DeliveryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => DeliveryProvider(),
+      child: MaterialApp(
+        title: 'Delivery App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: DeliveryHome(),
+      ),
+    );
+  }
+}
